a25b6fd325f35c6ccc278d2be60178b3175d6523
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-05-23 18:05:58 -0300

    Create dummy hero for testing

---
+++ heroquest/src/character/hero/Dummy.java
@@ -0,0 +1,13 @@
+package character.hero;
+
+import map.Map;
+import character.hero.Hero;
+
+public class Dummy extends Hero {
+
+    public Dummy(Map map, int X, int Y, int movementDice) {
+
+        super(map, X, Y, movementDice, "DY", "Dummy");
+    }
+
+}
