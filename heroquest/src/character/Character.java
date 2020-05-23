161fcef843880aeaf623bc676906735f7ef6bb01
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-05-23 18:05:15 -0300

    Create base character class

---
+++ heroquest/src/character/Character.java
@@ -0,0 +1,49 @@
+package character;
+
+import heroquest.map;
+
+public abstract class Character {
+
+    protected Map map;
+    protected int X;
+    protected int Y;
+    protected int attackDice;
+    protected int defenseDice;
+    protected int movementDice;
+    protected int bodyPoints;
+    protected int mindPoints;
+    protected String sprite;
+//    private Weapon weapon;
+//    private Spell[] spells;
+
+    public Character(Map map, int X, int Y, int movementDice, String sprite) {
+        this.map = map;
+        this.X = X;
+        this.Y = Y;
+        this.movementDice = movementDice;
+        this.sprite = sprite;
+    }
+
+    public abstract void move();
+
+    public abstract void attack();
+
+    public abstract void defend();
+
+    protected abstract void takeDamage();
+
+    protected abstract void castSpell();
+
+    protected String getSprite() {
+        return this.sprite;
+    }
+
+    public String getStatus() {
+        String s = String.format("Positon: (%d, %d), Attack dice: %d, " +
+                "Defense dice: %d, Body points: %d, Mind points: %d",
+                this.X, this.Y, this.attackDice, this.defenseDice,
+                this.bodyPoints, this.mindPoints);
+        return s;
+    }
+
+}
