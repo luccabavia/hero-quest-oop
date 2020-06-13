4272a7ef7f74954db967c90d2c6ff859a116da7f
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-06-12 22:16:25 -0300

    Add doc comments

--- heroquest/src/io/Display.java
+++ heroquest/src/io/Display.java
@@ -1,7 +1,14 @@
 package io;
 
+/**
+ * Manage output to display operations
+ */
 public class Display {
 
+    /**
+     * Prints strings to standard output.
+     * @param string String
+     */
     public static void print(String string) {
         System.out.println(string);
     }
