8cfc8ce240dc80868844605a36c414594aa00345
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-12 18:48:12 -0300

    Add javadoc to enum class

--- heroquest/src/dice/CombatDiceType.java
+++ heroquest/src/dice/CombatDiceType.java
@@ -1,5 +1,9 @@
 package dice;
 
+/**
+ * Simulates the behaviour of  combat dice, generating randomized outputs for
+ * combat behavior
+ */
 public enum CombatDiceType {
     SKULL, BLACK_SHIELD, WHITE_SHIELD;
 }
