7596ab55220683c204994ea4f2d248ebc471f216
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-12 18:50:30 -0300

    Create exceptions for map-character interaction

--- heroquest/src/exceptions/CannotWalkOverException.java
+++ heroquest/src/exceptions/CannotWalkOverException.java
@@ -1,4 +1,4 @@
-package map;
+package exceptions;
 
 public class CannotWalkOverException extends Exception {
 
--- heroquest/src/exceptions/PositionDoesNotExistException.java
+++ heroquest/src/exceptions/PositionDoesNotExistException.java
@@ -1,4 +1,4 @@
-package map;
+package exceptions;
 
 public class PositionDoesNotExistException extends Exception {
 
