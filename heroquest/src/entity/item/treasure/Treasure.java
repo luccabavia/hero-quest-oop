c83f729a093096b212cb35f62a00d984750d2ce7
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-12 18:52:50 -0300

    Implements item interface

--- heroquest/src/entity/item/treasure/Treasure.java
+++ heroquest/src/entity/item/treasure/Treasure.java
@@ -1,15 +1,20 @@
 package entity.item.treasure;
 
-public abstract class Treasure {
+import entity.item.Item;
+
+public abstract class Treasure implements Item {
 
     private int value;
 
     public Treasure(int value) {
-
+        this.value = value;
     }
 
     public int getValue() {
         return this.value;
     }
 
+    public Item collect() {
+        return this;
+    }
 }
