c978ef7557f7508c8b8db27c92b3885b089d268a
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-12 21:09:44 -0300

    Fix merge conflict HEADER

--- heroquest/src/battle/Battle.java
+++ heroquest/src/battle/Battle.java
@@ -1,15 +1,12 @@
 package battle;
 
-<<<<<<< HEAD
 import entity.character.Character;
 import dice.*;
 import io.Keyboard;
-=======
+
 /**
  * Define combat interaction between two chacracter
  */
->>>>>>> 37f24fad485c5be04696c57cf938cff25b64e3d6
-
 public class Battle {
 
     private static Battle instance;
