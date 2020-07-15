cfbe291d4e2b23debb70a8769592d9a6d2998b08
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-14 23:07:07 -0300

    Add exception to main signature

--- heroquest/src/Runner.java
+++ heroquest/src/Runner.java
@@ -1,9 +1,11 @@
+import exceptions.UnknownItemException;
+
 /**
  * Class which initialize game  
  */
 public class Runner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownItemException {
 
         Game g = new Game();
         g.startGameLoop();
