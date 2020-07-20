3733c06742ea26dc8edd2a0fbfd494ff1e227c12
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-20 10:20:52 -0300

    WIP: Implement SpellCaster interface (castSpell to be implemented), implement setStartingEquipment and getEquippedSpellRange

--- heroquest/src/entity/character/monster/SkeletonMage.java
+++ heroquest/src/entity/character/monster/SkeletonMage.java
@@ -1,30 +1,41 @@
 package entity.character.monster;
 
+import entity.character.SpellCaster;
 import item.equipment.spell.MagicMissile;
+import item.equipment.spell.Spell;
 import item.equipment.weapon.Fists;
 import map.Map;
 
-public class SkeletonMage extends Monster {
+import java.util.ArrayList;
+
+public class SkeletonMage extends Monster implements SpellCaster {
+
+    private ArrayList<Spell> spells = new ArrayList<>();
 
     public SkeletonMage(Map map, int x, int y) {
         super(map, x, y, "SM", 2, 1, 3,
                 3);
-        this.weapon.add(new Fists());
-        this.spells.add(new MagicMissile());
+        this.setStartingEquipment();
     }
 
     @Override
-    public void attack() {
-
-    }
-
-    @Override
-    public void defend() {
+    protected void setStartingEquipment() {
+        this.weapon = new Fists();
+        for (int i = 0; i < 3; i++) {
+            this.spells.add(new MagicMissile());
+        }
 
     }
 
     @Override
-    protected void castSpell() {
+    public int getEquippedSpellRange() {
+        return this.spells.get(0).getRange();
+    }
+
+    @Override
+    public void castSpell() {
 
     }
+
+
 }
