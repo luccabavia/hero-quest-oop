0277877f0a7c7b8345e1114d815c8c003b59617f
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-20 10:17:02 -0300

    Implement getAttack, getDefense and getEquippedWeaponRange methods

--- heroquest/src/entity/character/monster/Monster.java
+++ heroquest/src/entity/character/monster/Monster.java
@@ -2,20 +2,38 @@ package entity.character.monster;
 
 import exceptions.*;
 import io.Display;
+import item.equipment.spell.Spell;
+import item.equipment.weapon.Weapon;
 import map.Map;
 import entity.character.Character;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public abstract class Monster extends Character {
 
     protected int maxSteps;
+    protected Weapon weapon;
 
     Monster(Map map, int x, int y, String sprite, int bodyPoints,
             int attackDice, int defenseDice, int maxSteps) {
         super(map, x, y, String.format("\u001b[35m%s\u001b[0m",sprite),
                 bodyPoints, attackDice, defenseDice);
         this.maxSteps = maxSteps;
+    }
+
+    @Override
+    public int getAttack() {
+        return this.attackDice + this.weapon.getAttack();
+    }
+
+    public int getDefense() {
+        return this.defenseDice;
+    }
+
+    @Override
+    public int getEquippedWeaponRange() {
+        return this.weapon.getRange();
     }
 
     public void move() {
