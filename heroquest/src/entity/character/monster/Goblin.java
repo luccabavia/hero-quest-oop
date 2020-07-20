3d236b76145bf229cb023f508474da3608c617c4
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-20 10:19:23 -0300

    Add particular implementation for getAttack and setStartingEquipment, and refactor stored weapons

--- heroquest/src/entity/character/monster/Goblin.java
+++ heroquest/src/entity/character/monster/Goblin.java
@@ -12,10 +12,20 @@ import java.util.Random;
 
 public class Goblin extends Monster {
 
+    private ArrayList<Weapon> weaponPocket = new ArrayList<>();
+
     public Goblin(Map map, int x, int y) {
         super(map, x, y, "Go", 4, 3,1,
                 5);
-        this.weapon.add(new Dagger());
+        this.setStartingEquipment();
+    }
+
+    protected void setStartingEquipment() {
+        int maxDaggers = 6;
+        for (int i = 0; i < maxDaggers; i++) {
+            weaponPocket.add(new Dagger());
+        }
+        this.weapon = this.weaponPocket.remove(0);
     }
 
     @Override
@@ -99,17 +109,10 @@ public class Goblin extends Monster {
     }
 
     @Override
-    public void attack() {
-
+    public int getAttack() {
+        Weapon usedDagger = this.weapon;
+        this.weapon = this.weaponPocket.remove(0);
+        return this.attackDice + usedDagger.performAttack();
     }
 
-    @Override
-    public void defend() {
-
-    }
-
-    @Override
-    protected void castSpell() {
-
-    }
 }
