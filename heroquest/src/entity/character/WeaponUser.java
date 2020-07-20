917012824d0dbc2aba42798237c825f29ac273a8
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-20 09:51:36 -0300

    Create interface for characters that can use weapons

---
+++ heroquest/src/entity/character/WeaponUser.java
@@ -0,0 +1,11 @@
+package entity.character;
+
+public interface WeaponUser {
+
+    /**
+     * Get equipped weapon's range.
+     * @return int equipped weapon's range
+     */
+    int getEquippedWeaponRange();
+
+}
