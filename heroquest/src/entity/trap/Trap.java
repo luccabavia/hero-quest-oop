2e5994d5a565407e0f900b8be43774fb315a7ae8
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-21 09:47:44 -0300

    Integrate Igor's trap class

---
+++ heroquest/src/entity/trap/Trap.java
@@ -0,0 +1,18 @@
+package entity.trap;
+
+import entity.Entity;
+
+public class Trap extends Entity{
+    private int damage;
+
+    public Trap(String sprite, int x, int y, int damage) {
+        this.sprite = "Trap";
+        this.x = x;
+        this.y = y;
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
+}
