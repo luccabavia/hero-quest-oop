6ab3889f70233b1d96ce8b1ca6c965fb1db6f28c
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-21 09:37:24 -0300

    WIP: implement cast method

--- heroquest/src/item/equipment/spell/Teleport.java
+++ heroquest/src/item/equipment/spell/Teleport.java
@@ -8,12 +8,17 @@ import java.util.Random;
 
 public class Teleport extends Spell implements TransportSpell {
 
+    private int[] destination;
+
     public Teleport() {
         super(1, 0, "Teleport", 6);
     }
 
     @Override
     public void castSpell(int x, int y) {
+
+
+//        int[] dupla = alguma_coisa_que_pede_input();
 
     }
 }
