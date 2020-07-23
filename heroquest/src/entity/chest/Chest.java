e2709ebb5710622e99f29882399d4e5a2f6c41d8
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-23 09:43:26 -0300

    Refactor into abstract base class

--- heroquest/src/entity/chest/Chest.java
+++ heroquest/src/entity/chest/Chest.java
@@ -1,45 +1,21 @@
 package entity.chest;
 
 import entity.Entity;
+import exceptions.MonsterHiddenInChestException;
 import io.Display;
 import item.Item;
 
 import java.util.ArrayList;
 
-public class Chest extends Entity {
+public abstract class Chest extends Entity {
 
-    private ArrayList<Item> items = new ArrayList<>();
-    private boolean hasMonster;
-
-    public Chest(int x, int y) {
+    protected Chest(int x, int y) {
         this.x = x;
         this.y = y;
         this.sprite = "Ch";
+        this.hidden = false;
     }
 
-    public void addItem(Item item) {
-        items.add(item);
-    }
-
-    public void displayItems() {
-        StringBuilder s = new StringBuilder();
-        int index = 0;
-        for (Item i : items) {
-            s.append(String.format("[%d] %s", index, i.getName()));
-            if (index != items.size()) {s.append("; ");}
-            index++;
-        }
-        Display.print(s.toString());
-    }
-
-    public int getSize() {
-        return items.size();
-    }
-
-    public Item collectItems(int itemIndex) {
-        Item i = items.get(itemIndex);
-        items.remove(itemIndex);
-        return i;
-    }
+    public abstract boolean hasContents() throws MonsterHiddenInChestException;
 
 }
