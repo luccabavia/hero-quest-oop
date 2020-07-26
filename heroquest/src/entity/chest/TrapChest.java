355c83b88454219ff10f855f05f014678132188d
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-26 16:41:50 -0300

    Refactor: move mosnter creation responsibility to monster generator inside of map

--- heroquest/src/entity/chest/TrapChest.java
+++ heroquest/src/entity/chest/TrapChest.java
@@ -1,43 +1,19 @@
 package entity.chest;
 
-import entity.character.monster.MonsterType;
 import exceptions.MonsterHiddenInChestException;
-import java.util.Random;
 
 public class TrapChest extends Chest {
 
-    private boolean randomized;
 
-    public TrapChest(int x, int y, boolean randomized) {
+    public TrapChest(int x, int y) {
         super(x, y);
-        this.randomized = randomized;
     }
 
     @Override
     public boolean hasContents() throws MonsterHiddenInChestException {
-        MonsterType monsterType;
-        if (this.randomized) {
-            Random random = new Random();
-            switch (random.nextInt(3)) {
-                case 1:
-                    monsterType = MonsterType.GOBLIN;
-                    break;
-                case 2:
-                    monsterType = MonsterType.SKELETON_MAGE;
-                    break;
-                default:
-                    monsterType = MonsterType.SKELETON;
-            }
-        } else {
-            monsterType = MonsterType.SKELETON;
-        }
-        String message = String.format(
-                "The chest had a monster inside it! It " +
-                "was a %s!", monsterType.name()
-        );
+        String message = "The chest had a monster inside it!";
         throw new MonsterHiddenInChestException(
                 message,
-                monsterType,
                 new int[] {this.x, this.y}
         );
     }
