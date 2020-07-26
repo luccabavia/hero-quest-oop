45c6ee955d5b3cc05a2bb91c838f6593958c6de7
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-26 17:49:50 -0300

    Implement castSpell method

--- heroquest/src/item/equipment/spell/Teleport.java
+++ heroquest/src/item/equipment/spell/Teleport.java
@@ -1,24 +1,51 @@
 package item.equipment.spell;
 
 import entity.character.Character;
-import io.Keyboard;
-import map.MovementDirection;
+import exceptions.CannotWalkOverException;
+import exceptions.IsTrapException;
+import exceptions.PositionDoesNotExistException;
+import io.Display;
+import map.Map;
+import target.Target;
 
-import java.util.Random;
+public class Teleport extends Spell {
 
-public class Teleport extends Spell implements TransportSpell {
+    private Map map;
 
-    private int[] destination;
-
-    public Teleport() {
-        super(1, 0, "Teleport", 6);
+    public Teleport(Map map) {
+        super(1, 0, "Teleport", 6,
+                SpellEffectType.TRANSPORT, 1);
+        this.map = map;
     }
 
     @Override
-    public void castSpell(int x, int y) {
+    public void castSpell(Target target) {
 
+        Character character = target.getCharacter();
+        int[] position = target.getPosition();
 
-//        int[] dupla = alguma_coisa_que_pede_input();
+        try {
+            if (this.map.isVisible(position[0], position[1])
+                    && this.map.isAvailable(position[0], position[1])) {
+                int[] oldPosition = character.getPosition();
+                character.setPosition(position[0], position[1]);
+                this.map.updateMap(oldPosition[0], oldPosition[1]);
+                this.casts--;
+            } else {
+                Display.printWarning("Cannot teleport to this position: " +
+                        "it is not visible");
+            }
+        } catch (PositionDoesNotExistException | CannotWalkOverException e) {
+            Display.printWarning(e.getMessage());
+        } catch (IsTrapException e) {
+            Display.printWarning(e.getMessage());
+            int damage = e.getDamage();
+            int[] newPosition = e.getTrapPosition();
+            character.sufferEffect(-damage);
+            this.map.disarmTrap(newPosition[0], newPosition[1]);
+            Display.print(character.getStatus());
+            this.map.updateVisibility();
+        }
 
     }
 }
