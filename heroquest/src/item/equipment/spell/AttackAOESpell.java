8c577b5442b35cd0f730d0b17dedce26560aa90d
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-26 17:47:30 -0300

    Remove unecessary interfaces

--- heroquest/src/item/equipment/spell/AttackAOESpell.java
+++
@@ -1,9 +0,0 @@
-package item.equipment.spell;
-
-import entity.character.Character;
-
-public interface AttackAOESpell {
-
-    void castSpell(Character character);
-
-}
--- heroquest/src/item/equipment/spell/BuffingSpell.java
+++
@@ -1,9 +0,0 @@
-package item.equipment.spell;
-
-import entity.character.Character;
-
-public interface BuffingSpell {
-
-    void castSpell(Character target);
-
-}
