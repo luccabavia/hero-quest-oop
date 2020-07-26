4bf796104a48338cc274bcf69e27bf7d397ba085
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-26 16:27:10 -0300

    Fix switch-case statement and indentation

--- heroquest/src/map/generator/MonsterGenerator.java
+++ heroquest/src/map/generator/MonsterGenerator.java
@@ -1,70 +1,94 @@
-package generator;
+package map.generator;
 
-import entity.Entity;
 import entity.character.monster.*;
 import io.Display;
 import map.Map;
 import exceptions.*;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.HashMap;
+import java.util.Random;
 import java.util.Set;
 
 public class MonsterGenerator {
 
-    protected Map map;
+    private Map map;
+    private Random randGenerator = new Random();
 
     public MonsterGenerator(Map map) {
         this.map = map;
     }
 
-    public Entity generateNewEntity(MonsterType monsterType, int[] position)
+    private void generateEntity(MonsterType monsterType, int[] position)
             throws InvalidGeneratorSeedException {
 
         try {
             if (this.map.isAvailable(position[0], position[1])) {
                 switch (monsterType) {
                     case SKELETON:
-                        return new Skeleton(map, position[0], position[1]);
+                        this.map.setEntity(new Skeleton(map, position[0],
+                                position[1]));
+                        break;
                     case SKELETON_MAGE:
-                        return new SkeletonMage(map, position[0], position[1]);
+                        this.map.setEntity(new SkeletonMage(map, position[0]
+                                , position[1]));
+                        break;
                     case GOBLIN:
-                        return new Goblin(map, position[0], position[1]);
+                        this.map.setEntity(new Goblin(map, position[0],
+                                position[1]));
+                        break;
                 }
             }
         } catch (PositionDoesNotExistException
                 | IsTrapException
                 | CannotWalkOverException e) {
             throw new InvalidGeneratorSeedException(
-                    String.format("Cannot place entity at " +
-                    "position: %s", e.getMessage())
+                    String.format("Cannot place entity at " + 
+                    "position: %d %d", position[0], position[1])
             );
         }
-
-        return null;
-
     }
 
-    public ArrayList<Entity> generateMultipleEntities(
-            HashMap<MonsterType, int[][]> hashMap) throws
-            InvalidGeneratorSeedException {
+    public void generateMultipleEntities(
+            HashMap<MonsterType, int[][]> hashMap) {
 
         Set<MonsterType> keys = hashMap.keySet();
         MonsterType[] monsterTypes = new MonsterType[keys.size()];
         monsterTypes = keys.toArray(monsterTypes);
 
-        ArrayList<Entity> entities = new ArrayList<>();
         for (MonsterType type : monsterTypes) {
             int[][] positions = hashMap.get(type);
             for (int[] position : positions) {
-                entities.add(this.generateNewEntity(type,
-                        position));
+                try {
+                    this.generateEntity(type, position);
+                } catch (InvalidGeneratorSeedException e) {
+                    Display.printWarning(e.getMessage());
+               }
             }
         }
-        return entities;
+    }
 
+    public void generateMultipleRandomEntities(
+            int maxEntitiesNumber) {
+        int remainingEntities= maxEntitiesNumber;
+        do {
+            MonsterType type =
+                    MonsterType.values()[
+                            randGenerator.nextInt(
+                                    MonsterType.values().length)
+                            ];
+            int[] position = new int[] {
+                    randGenerator.nextInt(this.map.getMapSize()[0]),
+                    randGenerator.nextInt(this.map.getMapSize()[1])
+            };
+
+            try {
+                remainingEntities--;
+                this.generateEntity(type, position);
+            } catch (InvalidGeneratorSeedException e) {
+                remainingEntities++;
+            }
+
+        } while(remainingEntities > 0);
     }
 
 }
