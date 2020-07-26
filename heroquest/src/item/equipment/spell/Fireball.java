423cababfe9d9ae71ed73c8a468d2e7fbb392c3c
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-26 17:48:02 -0300

    Implement castSpell method

--- heroquest/src/item/equipment/spell/Fireball.java
+++ heroquest/src/item/equipment/spell/Fireball.java
@@ -1,17 +1,39 @@
 package item.equipment.spell;
 
 import entity.character.Character;
+import entity.character.monster.Monster;
+import map.Map;
+import target.Target;
 
-public class Fireball extends Spell implements AttackAOESpell {
+import java.util.ArrayList;
 
-    private final int DAMAGE = -6;
+public class Fireball extends AttackSpell {
 
-    public Fireball() {
-        super(1, -6, "Fireball", 6);
+    private final int AREA_OF_EFFECT_DAMAGE = 3;
+    private Map map;
+
+    public Fireball(Map map) {
+        super(1, 6, "Fireball", 6,
+                SpellEffectType.ATTACK, 1);
+        this.map = map;
     }
 
     @Override
-    public void castSpell(Character target) {
-        target.sufferEffect(DAMAGE);
+    public void castSpell(Target target) {
+        Character targetChar = target.getCharacter();
+        this.damageTarget(targetChar, lifeEffect);
+
+        int[] targetsPosition = targetChar.getPosition();
+        this.aoeDamage(targetsPosition[0], targetsPosition[1]);
+
+        this.casts--;
+    }
+
+    private void aoeDamage(int x, int y) {
+        ArrayList<Monster> monster =
+                this.map.getCharactersAround(x, y);
+        for (Monster m: monster) {
+            this.damageTarget(m, AREA_OF_EFFECT_DAMAGE);
+        }
     }
 }
