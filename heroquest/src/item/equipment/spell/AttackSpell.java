e643f5cd587df34425d67e1caba8c2b19f8579dc
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-26 17:45:48 -0300

    Implement damage target method

--- heroquest/src/item/equipment/spell/AttackSpell.java
+++ heroquest/src/item/equipment/spell/AttackSpell.java
@@ -1,12 +1,20 @@
 package item.equipment.spell;
 
+import dice.Dice;
 import entity.character.Character;
 
-public interface AttackSpell {
+public abstract class AttackSpell extends Spell {
 
-    void castSpell(Character target);
-        //target.sufferEffect(this.lifeEffect);
 
-    void castSpell(Character[] targets);
+    public AttackSpell(int casts, int lifeEffect, String name,
+                       int range, SpellEffectType type, int maxTargets) {
+        super(casts, lifeEffect, name, range, type, maxTargets);
+    }
+
+    protected void damageTarget(Character target, int damage) {
+        int defense = Dice.rollCombatDice(target.getMindPoints(),
+                target.getDefenseType());
+        target.sufferEffect(Math.min(0, -(damage - defense)));
+    }
 
 }
