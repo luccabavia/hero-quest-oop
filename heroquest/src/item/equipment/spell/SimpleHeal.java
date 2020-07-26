02373893e86ee7a44a1d2aab9ecf34ac7c3c0b11
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-26 17:49:17 -0300

    Extend Spell class and implement castSpell

--- heroquest/src/item/equipment/spell/SimpleHeal.java
+++ heroquest/src/item/equipment/spell/SimpleHeal.java
@@ -1,21 +1,22 @@
 package item.equipment.spell;
 
 import dice.Dice;
-import entity.character.Character;
+import target.Target;
 
-public class SimpleHeal extends Spell implements BuffingSpell {
+public class SimpleHeal extends Spell {
 
     private final int NUMBER_OF_DICE = 1;
-    private Dice dice;
 
     public SimpleHeal() {
-        super(1, 1, "SimpleHeal", 0);
+        super(1, 1,
+                "SimpleHeal", 0, SpellEffectType.BUFFING, 1);
     }
 
     @Override
-    public void castSpell(Character target) {
+    public void castSpell(Target target) {
         int healValue =
                 this.lifeEffect * Dice.rollRedDice(this.NUMBER_OF_DICE);
-        target.sufferEffect(healValue);
+        target.getCharacter().sufferEffect(healValue);
+        this.casts--;
     }
 }
