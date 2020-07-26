dbe947218a3493e09af96e27ecc64579bfdf84f0
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-26 16:19:25 -0300

    Refactor: add new constructor, define trapPosition and damage and getter methods

--- heroquest/src/exceptions/IsTrapException.java
+++ heroquest/src/exceptions/IsTrapException.java
@@ -2,6 +2,9 @@ package exceptions;
 
 public class IsTrapException extends Exception {
 
+    private int[] trapPosition;
+    private int damage;
+
     public IsTrapException() {
         super();
     }
@@ -18,4 +21,17 @@ public class IsTrapException extends Exception {
         super(message, cause);
     }
 
+    public IsTrapException(String message, int x, int y, int damage) {
+        super(message);
+        this.trapPosition = new int[] {x, y};
+        this.damage = damage;
+    }
+
+    public int[] getTrapPosition() {
+        return this.trapPosition;
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
 }
