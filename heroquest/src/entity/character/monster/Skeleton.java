714bd7a30234436f31eb6c838580af74eaa37ff5
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-26 17:56:35 -0300

    Add mindPoints attribute

--- heroquest/src/entity/character/monster/Skeleton.java
+++ heroquest/src/entity/character/monster/Skeleton.java
@@ -7,7 +7,7 @@ public class Skeleton extends Monster {
 
     public Skeleton(Map map, int x, int y) {
         super(map, x, y, "Sk", 4, 2,2,
-                2);
+                1,2);
         this.setStartingEquipment();
     }
 
