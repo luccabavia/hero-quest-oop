b4aab0cee07943d6654d271f57b0c46ddf289a26
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-26 18:05:08 -0300

    Implement SpellCaster methods, reimplement getStatus

--- heroquest/src/entity/character/hero/Wizard.java
+++ heroquest/src/entity/character/hero/Wizard.java
@@ -1,11 +1,16 @@
 package entity.character.hero;
 
-import item.Item;
+import entity.character.SpellCaster;
+import exceptions.*;
+import item.Collectible;
 import map.Map;
 import item.equipment.weapon.*;
 import item.equipment.spell.*;
+import target.Target;
 
-public class Wizard extends Hero {
+public class Wizard extends Hero implements SpellCaster {
+
+    private Spell equippedSpell;
 
     public Wizard(Map map, int x, int y, String name) {
         super(map, x, y, "Wz", 4,1, 2,
@@ -15,15 +20,94 @@ public class Wizard extends Hero {
     }
 
     @Override
+    public String getStatus() {
+        StringBuilder spellString = new StringBuilder();
+        if (this.equippedSpell != null) {
+            spellString.append(this.equippedSpell.getCharacteristics());
+        } else {
+            spellString.append("-");
+        }
+        String status = String.format("%s, \nSpell {%s}", super.getStatus(),
+                spellString.toString());
+        return status;
+    }
+
+    @Override
     protected void setStartingEquipment() {
         this.setRightHandWeapon(new Dagger());
-        /* Arrumar para ser tres punhais que
-           quando lança é perdido */
         this.setSpell(new MagicMissile());
-        Item[] items = new Item[] {new Dagger(), new Dagger(),
-                new MagicMissile(), new Fireball(), new Teleport()};
-        for (Item i : items) {
+        Collectible[] items = new Collectible[] {new MagicMissile(),
+                new MagicMissile(), new Fireball(this.map),
+                new Teleport(this.map)};
+        for (Collectible i : items) {
             this.addItemToBag(i);
         }
     }
+
+    /**
+     * Change equipped spell with other spell available in hero's bag
+     * @param spellIndexInBag spell's int index value in hero's bag
+     */
+    @Override
+    public void setEquippedSpell(int spellIndexInBag) throws
+            InvalidItemException {
+        try {
+            Spell newSpell = (Spell) this.bag.getItem(spellIndexInBag);
+            this.setSpell(newSpell);
+            this.bag.removeItem(spellIndexInBag);
+        } catch (ClassCastException e) {
+            throw new InvalidItemException("This item is not a spell!");
+        }
+    }
+
+    @Override
+    public void setSpell(Spell spell) {
+        if (this.equippedSpell != null) {
+            this.addItemToBag(this.equippedSpell);
+            this.equippedSpell = null;
+        }
+        this.equippedSpell = spell;
+    }
+
+    @Override
+    public void hasSpells() throws NoSpellLeftException {
+        if (this.equippedSpell == null) {
+            for (int i = 0; i < this.bag.getSize(); i++) {
+                try {
+                    Spell spell = (Spell) this.bag.getItem(i);
+                    return;
+                } catch (ClassCastException e) {
+                    continue;
+                }
+            }
+            throw new NoSpellLeftException("No spells remaining!");
+        } 
+    }
+
+    @Override
+    public int getEquippedSpellMaxTargets() {
+        return this.equippedSpell.getMaxSpellTargets();
+    }
+
+    @Override
+    public int getEquippedSpellRange() {
+        return this.equippedSpell.getRange();
+    }
+
+    @Override
+    public SpellEffectType getEquippedSpellType() {
+        return this.equippedSpell.getType();
+    }
+
+    @Override
+    public void castSpell(Target target) throws
+            CannotWalkOverException, PositionDoesNotExistException,
+            IsTrapException {
+        this.equippedSpell.castSpell(target);
+        if (!this.equippedSpell.hasCastsLeft()) {
+            this.equippedSpell = null;
+        }
+
+    }
+    
 }
