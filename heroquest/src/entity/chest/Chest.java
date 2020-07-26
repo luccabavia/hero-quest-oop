ac0418f621035be218a3bd40b0807e5adcf9b987
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-26 17:38:17 -0300

    Minor fixes

--- heroquest/src/entity/chest/Chest.java
+++ heroquest/src/entity/chest/Chest.java
@@ -2,18 +2,11 @@ package entity.chest;
 
 import entity.Entity;
 import exceptions.MonsterHiddenInChestException;
-import io.Display;
-import item.Item;
-
-import java.util.ArrayList;
 
 public abstract class Chest extends Entity {
 
     protected Chest(int x, int y) {
-        this.x = x;
-        this.y = y;
-        this.sprite = "Ch";
-        this.hidden = false;
+        super(x, y, "Ch", false, false);
     }
 
     public abstract boolean hasContents() throws MonsterHiddenInChestException;
