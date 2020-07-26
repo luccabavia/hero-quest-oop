71c19f3b175cae11da4bc8b8e9169bcb070cff3c
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-26 18:08:50 -0300

    Implement potion usage, attack based on equipped weapon, check for potion in bag

--- heroquest/src/entity/character/hero/Hero.java
+++ heroquest/src/entity/character/hero/Hero.java
@@ -2,14 +2,13 @@ package entity.character.hero;
 
 import bag.Bag;
 import dice.CombatDiceType;
-import entity.character.SpellCaster;
+import entity.character.*;
 import entity.chest.Chest;
 import exceptions.*;
 import io.Display;
-import io.Keyboard;
-import item.Item;
+import item.Collectible;
 import item.equipment.armor.Armor;
-import item.equipment.spell.Spell;
+import item.equipment.potion.Potion;
 import item.equipment.weapon.Weapon;
 import map.Map;
 import entity.character.Character;
@@ -17,20 +16,16 @@ import entity.character.Character;
 /**
  * Parent class for any kind of Hero that can be created at the game.
  */
-public abstract class Hero extends Character implements SpellCaster {
+public abstract class Hero extends Character implements ArmorUser {
 
-
-    
     private String name;
-    private int mindPoints;
     private int movementDice;
     private Armor equippedArmor;
     private Weapon leftHandWeapon = null;
     private Weapon rightHandWeapon = null;
     private Weapon dualWieldingWeapon = null;
     private OccupiedHand usedHand;
-    private Spell equippedSpell;
-    private Bag bag;
+    protected Bag bag;
 
     /**
      * Constructor method for hero parent class.
@@ -49,9 +44,8 @@ public abstract class Hero extends Character implements SpellCaster {
                 int attackDice, int defenseDice, int movementDice,
                 int mindPoints, String name) {
         super(map, x, y, sprite, bodyPoints, attackDice,
-                defenseDice, CombatDiceType.WHITE_SHIELD);
+                defenseDice, mindPoints, CombatDiceType.WHITE_SHIELD);
         this.name = name;
-        this.mindPoints = mindPoints;
         this.movementDice = movementDice;
         this.bag = new Bag();
     }
@@ -84,18 +78,22 @@ public abstract class Hero extends Character implements SpellCaster {
         return null;
     }
 
-    public void addItemToBag(Item item) {
+    public void addItemToBag(Collectible item) {
         this.bag.addItem(item);
     }
 
     public String getItemsInBag() {
         return this.bag.displayItems();
     }
+    
+    public boolean hasPotionInBag() {
+    	return this.bag.hasPotion();
+    }
 
     @Override
     public String getStatus() {
 
-        String weapon="-", spell="-", armor= "-";
+        String weapon="-", armor= "-";
         if (this.dualWieldingWeapon != null) {
             weapon = this.dualWieldingWeapon.getCharacteristics();
         } else {
@@ -110,19 +108,14 @@ public abstract class Hero extends Character implements SpellCaster {
             weapon = String.format("Left hand weapon: %s, Right hand weapon:" +
                     " %s", leftWeapon, rightWeapon);
         }
-        if (this.equippedSpell != null) {
-            spell = this.equippedSpell.getCharacteristics();
-        }
         if (this.equippedArmor != null) {
             armor = this.equippedArmor.getCharacteristics();
         }
         String s = String.format("Name: %s, %s, Movement dice: %d, " +
-                        "Mind points: %s, " +
-                        "\nEquipment{ Weapon: %s, Spell: %s, " +
-                        "Armor: %s}, " +
-                        "\nBag contents: %s",
+                        "\nEquipment{ Weapon: %s, Armor: %s}, " +
+                        "\nBag{ %s}",
                 this.name, super.getStatus(), this.movementDice,
-                this.mindPoints, weapon, spell, armor,
+                weapon, armor,
                 this.bag.getStatus());
         return s;
     }
@@ -147,31 +140,6 @@ public abstract class Hero extends Character implements SpellCaster {
     }
 
     /**
-     * Display weapons held in hands
-     * @return string with weapon info and which hand is holding which weapon
-     */
-    public String displayWeaponsInHands() {
-        StringBuilder s = new StringBuilder();
-        if (this.leftHandWeapon.equals(this.rightHandWeapon)) {
-            s.append(String.format("Weapon held with both hands: %s",
-                    this.leftHandWeapon.getDescription())
-            );
-        } else {
-            if (this.leftHandWeapon != null) {
-                s.append(String.format("Left hand: %s\n",
-                        this.leftHandWeapon.getCharacteristics())
-                );
-            }
-            if (this.rightHandWeapon != null) {
-                s.append(String.format("Right hand: %s\n",
-                        this.rightHandWeapon.getCharacteristics())
-                );
-            }
-        }
-        return s.toString();
-    }
-
-    /**
      * Get attack dice number based in weapon chosen
      * @param occupiedHand hand selected for next attack
      * @return base character attack value plus with weapon attack value
@@ -181,13 +149,31 @@ public abstract class Hero extends Character implements SpellCaster {
         int weaponAttackBonus = 0;
         switch (occupiedHand) {
             case LEFT:
-                weaponAttackBonus = this.leftHandWeapon.performAttack();
+                weaponAttackBonus = this.leftHandWeapon.getAttack();
+                if (this.leftHandWeapon.getDurability() <= 0) {
+                    this.leftHandWeapon = null;
+                    Display.printWarning("Your weapon is broken! " +
+                            "Remember to" +
+                            " equip another in the next round!");
+                }
                 break;
             case RIGHT:
-                weaponAttackBonus = this.rightHandWeapon.performAttack();
+                weaponAttackBonus = this.rightHandWeapon.getAttack();
+                if (this.rightHandWeapon.getDurability() <= 0) {
+                    this.rightHandWeapon = null;
+                    Display.printWarning("Your weapon is broken! " +
+                            "Remember to" +
+                            " equip another in the next round!");
+                }
                 break;
             case BOTH:
-                weaponAttackBonus = this.dualWieldingWeapon.performAttack();
+                weaponAttackBonus = this.dualWieldingWeapon.getAttack();
+                if (this.dualWieldingWeapon.getDurability() <= 0) {
+                    this.dualWieldingWeapon = null;
+                    Display.printWarning("Your weapon is broken! " +
+                            "Remember to" +
+                            " equip another in the next round!");
+                }
                 break;
         }
         return this.attackDice + weaponAttackBonus;
@@ -207,40 +193,12 @@ public abstract class Hero extends Character implements SpellCaster {
         return weaponRange;
     }
 
-    @Override
-    public int getEquippedSpellRange() {
-        return this.equippedSpell.getRange();
-    }
-
     /**
      * Set weapon hand used for next attack
      * @param hand chosen hero's hand
      */
     public void setUsedHand(OccupiedHand hand) {
         this.usedHand = hand;
-    }
-
-    /**
-     * Change equipped spell with other spell available in hero's bag
-     * @param spellIndexInBag spell's int index value in hero's bag
-     */
-    public void setEquippedSpell(int spellIndexInBag) throws
-            InvalidItemException {
-        try {
-            Spell newSpell = (Spell) this.bag.getItem(spellIndexInBag);
-            this.setSpell(newSpell);
-        } catch (ClassCastException e) {
-            throw new InvalidItemException("This item cannot be equipped in " +
-                    "this slot!");
-        }
-    }
-
-    protected void setSpell(Spell spell) {
-        if (this.equippedSpell != null) {
-            this.addItemToBag(this.equippedSpell);
-            this.equippedSpell = null;
-        }
-        this.equippedSpell = spell;
     }
 
     /**
@@ -252,9 +210,9 @@ public abstract class Hero extends Character implements SpellCaster {
         try {
             Armor newArmor = (Armor) this.bag.getItem(armorIndexInBag);
             this.setArmor(newArmor);
+            this.bag.removeItem(armorIndexInBag);
         } catch (ClassCastException e) {
-            throw new InvalidItemException("This item cannot be equipped in " +
-                    "this slot!");
+            throw new InvalidItemException("This item is not an armor!");
         }
     }
 
@@ -293,9 +251,11 @@ public abstract class Hero extends Character implements SpellCaster {
                     setDualWieldingWeapon(newWeapon);
                     break;
             }
+            this.bag.removeItem(weaponIndexInBag);
 
         } catch (ClassCastException e) {
-            throw new InvalidItemException();
+            throw new InvalidItemException(
+                    "This item is not a weapon!");
         }
     }
 
@@ -359,29 +319,39 @@ public abstract class Hero extends Character implements SpellCaster {
         }
     }
 
+    @Override
+    public int getArmorDefense() {
+        int armorDefense = 0;
+        if (this.equippedArmor != null) {
+            armorDefense = this.equippedArmor.getDefense();
+        }
+        return armorDefense;
+    }
+
     /**
      * Get character's defense value
      * @return character's defense value as an int
      */
     @Override
     public int getDefense() {
-        int armorDefense = 0;
-        if (this.equippedArmor != null) {
-            armorDefense = this.equippedArmor.getDefense();
-        }
-        return this.defenseDice + armorDefense;
+        return this.defenseDice + this.getArmorDefense();
     }
 
     @Override
     public int getAttack() {
-
         return this.getAttackDice(this.usedHand);
     }
 
-    @Override
-    public void castSpell() {
+    public void usePotion(int bagIndex) throws InvalidItemException {
+        try {
+            this.sufferEffect(((Potion) this.bag.getItem(bagIndex)).usePotion());
+            this.bag.removeItem(bagIndex);
+        } catch (ClassCastException e) {
+            throw new InvalidItemException("This item is not a potion!");
+        }
 
     }
+
 }
 
 
