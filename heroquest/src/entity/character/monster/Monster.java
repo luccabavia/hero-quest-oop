0afe1522d907235a7b2b1b1f40cf6ebcaf3f6726
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-26 18:01:28 -0300

    Add mindPoints attribute

--- heroquest/src/entity/character/monster/Monster.java
+++ heroquest/src/entity/character/monster/Monster.java
@@ -2,13 +2,9 @@ package entity.character.monster;
 
 import dice.CombatDiceType;
 import exceptions.*;
-import io.Display;
-import item.equipment.spell.Spell;
 import item.equipment.weapon.Weapon;
 import map.Map;
 import entity.character.Character;
-
-import java.util.ArrayList;
 import java.util.Random;
 
 public abstract class Monster extends Character {
@@ -17,9 +13,9 @@ public abstract class Monster extends Character {
     protected Weapon weapon;
 
     Monster(Map map, int x, int y, String sprite, int bodyPoints,
-            int attackDice, int defenseDice, int maxSteps) {
+            int attackDice, int defenseDice, int mindPoints, int maxSteps) {
         super(map, x, y, String.format("\u001b[35m%s\u001b[0m",sprite),
-                bodyPoints, attackDice, defenseDice,
+                bodyPoints, attackDice, defenseDice, mindPoints,
                 CombatDiceType.BLACK_SHIELD);
         this.maxSteps = maxSteps;
     }
@@ -39,10 +35,8 @@ public abstract class Monster extends Character {
     }
 
     public void move() {
-//        System.out.printf("Initial (%d, %d)\n", this.x, this.y);
         Random random = new Random();
         int steps = random.nextInt(this.maxSteps) + 1;
-//        System.out.printf("Steps %d", steps);
         while (steps > 0) {
             try {
                 random = new Random();
@@ -62,19 +56,9 @@ public abstract class Monster extends Character {
                         this.moveWest();
                         break;
                 }
-//                this.map.drawMap();
-            } catch (PositionDoesNotExistException e){
-//                Display.printWarning(e.getMessage());
-                steps++;
-            } catch (CannotWalkOverException e) {
-//                Display.printWarning(e.getMessage());
-                steps++;
-            } catch (Exception e) {
-//                Display.printWarning(e.getMessage());
+            } catch (PositionDoesNotExistException | CannotWalkOverException | IsTrapException e) {
                 steps++;
             }
-//            System.out.printf("Step %d ; (%d, %d)", steps, this.x, this.y);
-//            System.out.println("");
 
         }
     }
