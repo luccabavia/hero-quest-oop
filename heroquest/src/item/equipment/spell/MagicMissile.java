f16d37d012785c1ce3810c60cd1a666dd3ba1d02
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-26 17:48:38 -0300

    Implement castSpell method

--- heroquest/src/item/equipment/spell/MagicMissile.java
+++ heroquest/src/item/equipment/spell/MagicMissile.java
@@ -1,46 +1,45 @@
 package item.equipment.spell;
 
-import entity.character.Character;
+import target.Target;
+
 import java.util.Random;
 
-public class MagicMissile extends Spell implements AttackSpell {
-
-
-    private final int MAX_TARGETS = 3;
+public class MagicMissile extends AttackSpell {
 
     public MagicMissile() {
-        super(1, -2, "MagicMissile", 3);
+        super(1, 2, "MagicMissile",
+                3, SpellEffectType.MULTI_ATTACK, 3);
     }
 
     @Override
-    public void castSpell(Character target) {
-        target.sufferEffect(MAX_TARGETS*lifeEffect);
-    }
+    public void castSpell(Target target) {
+        int numberOfTargets = target.getSize();
+        if (numberOfTargets == 1) {
+            this.damageTarget(
+                    target.getCharacter(),
+                    maxTargets * lifeEffect
+            );
+        } else if (numberOfTargets == this.maxTargets) {
+            for (int i = 0; i < maxTargets; i++) {
+                this.damageTarget(
+                        target.getCharacter(),
+                        1 * lifeEffect
+                );
+            }
+        } else {
+            Random rand = new Random();
+            int firstTarget = rand.nextInt(2) + 1;
+            int secondTarget = 3 - firstTarget;
 
-    @Override
-    public void castSpell(Character[] targets) {
-        int numberOfTargets = targets.length;
-        switch (numberOfTargets) {
-            case 1:
-                this.castSpell(targets[0]);
-                break;
-            case 3:
-                for (int i = 0; i < MAX_TARGETS; i++) {
-                    targets[i].sufferEffect(lifeEffect);
-                }
-                break;
-            default:
-                Random rand = new Random();
-                int firstTarget = rand.nextInt(2) + 1;
-                int secondTarget = 3 - firstTarget;
-
-                for (int i = 0; i < firstTarget; i++) {
-                    targets[0].sufferEffect(lifeEffect);
-                }
-
-                for (int i = 0; i < secondTarget; i++) {
-                    targets[1].sufferEffect(lifeEffect);
-                }
+            this.damageTarget(
+                    target.getCharacter(),
+                    firstTarget * lifeEffect
+            );
+            this.damageTarget(
+                    target.getCharacter(),
+                    secondTarget * lifeEffect
+            );
         }
+        this.casts--;
     }
 }
